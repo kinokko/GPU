835c711800b266ba6d3f5965f0f3ad4d117fb306
zhijunl <zhijunl@asb9838nu-a13.csil.sfu.ca>
2015-11-30 22:57:28 -0800

    small modification

--- Source/histogram-equalization-gpu.cu
+++ Source/histogram-equalization-gpu.cu
@@ -1,5 +1,16 @@
 #include "hist-equ-gpu.cuh"
 
-__global__ void histogram_gpu(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin){
-	int x = blockIdx.x + threadIdx.x;
+__global__ void HistogramInitGpu (int * histOut, int offSet) {
+	int index = threadIdx.x + blockIdx.x + offSet;
+	histOut
+}
+
+__global__ void HistogramGpu(int * histOut, unsigned char * imgIn, int imgSize, int blockSize, int totalBlock){
+	int loop = 0;
+	int index = threadIdx.x
+			+ blockIdx.x * blockSize
+			+ blockSize * totalBlock * blockSize;
+	while (index < imgSize) {
+		atomicAdd(histOut[imgIn[index]], 1);
+	}
 }
