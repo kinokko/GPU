12a3ddf043262ea756e264c3a0dcec52b16a391e
zhijunl <zhijunl@asb9838nu-a16.csil.sfu.ca>
2015-12-02 10:47:14 -0800

    enhance loop

--- Source/histogram-equalization-gpu.cu
+++ Source/histogram-equalization-gpu.cu
@@ -1,16 +1,13 @@
 #include "hist-equ-gpu.h"
 
-__global__ void HistogramInitGpu(int * histOut, int blockSize) {
-	int index = threadIdx.x + blockIdx.x * blockSize;
-	histOut[index] = 0;
+__global__ void HistogramInitGpu(int * histOut, int histSize) {
+	for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < histSize; i += blockDim.x * gridDim.x) {
+		histOut[i] = 0;
+	}
 }
 
-__global__ void HistogramGpu(int * histOut, unsigned char * imgIn, int imgSize, int blockSize, int totalBlock){
-	int loop = 0;
-	int index = threadIdx.x
-			+ blockIdx.x * blockSize
-			+ blockSize * totalBlock * blockSize;
-	while (index < imgSize) {
+__global__ void HistogramGpu(int * histOut, unsigned char * imgIn, int imgSize, int threadsPerBlock, int blocksPerGrid){
+	for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < histSize; i += blockDim.x * gridDim.x) {
 		atomicAdd(&histOut[imgIn[index]], 1);
 	}
 }
