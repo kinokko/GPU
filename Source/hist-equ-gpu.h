b622908de709790be33bb5de1d02d1dc01f9eeae
zhijunl <zhijunl@asb9838nu-a14.csil.sfu.ca>
2015-12-02 14:53:19 -0800

    add missing header

---
+++ Source/hist-equ-gpu.h
@@ -0,0 +1,12 @@
+#ifndef HIST_EQU_COLOR_GPU_H
+#define HIST_EQU_COLOR_GPU_H
+
+#include "hist-equ.h"
+
+void HistTest(PGM_IMG img_in);
+void HistogramGPU(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin);
+
+__global__ void HistogramInitGpuAction(int * histOut, int histSize);
+__global__ void HistogramGpuAction(int * histOut, unsigned char * imgIn, int imgSize);
+
+#endif
