6650204d3645275e1e9b8f0535c8f9070e97302e
kinokko <linzhijun032@yahoo.com>
2015-12-02 18:02:08 -0800

    Going to break the code

--- Source/hist-equ-gpu.h
+++ Source/hist-equ-gpu.h
@@ -2,11 +2,14 @@
 #define HIST_EQU_COLOR_GPU_H
 
 #include "hist-equ.h"
+#include <cuda_runtime.h>
 
 void HistTest(PGM_IMG img_in);
 void HistogramGPU(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin);
+void HistogramEqualizationGpu(unsigned char * img_out, unsigned char * img_in, int * hist_in, int img_size, int nbr_bin);
 
 __global__ void MemsetGPU(int * histOut, int histSize);
 __global__ void HistogramGpuAction(int * histOut, unsigned char * imgIn, int imgSize);
+
 
 #endif
